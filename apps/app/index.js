275c671cda9894af990afde1d02dcf1aac78c44a
James South <james.south@gmail.com>
2024-06-26 12:53:32 +0100

    Test of basic workflow

--- apps/app/index.js
+++ apps/app/index.js
@@ -3,9 +3,12 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+const apiHost = process.env.API_HOST ?? 'api';
+const apiPort = process.env.API_PORT ?? '3000';
+
 app.get('/', (req, res) => {
   console.log('App get');
-  fetch('http://api:3000/heartbeat')
+  fetch(`http://${apiHost}:${apiPort}/heartbeat`)
     .then((response) => response.text())
     .then((heartbeat) => {
       res.send(`Hello World! API connection is: ${heartbeat}`);
--- docker/Dockerfile.app
+++ docker/Dockerfile.app
@@ -2,7 +2,7 @@ FROM node:22-alpine3.19
 
 EXPOSE 3000
 
-COPY apps/api /app
+COPY apps/app /app
 WORKDIR /app
 RUN npm ci
 
